b108fd01708e42ab4e7eb43519071d9c91ac54c0
Érick Firmo <erickfirmo1996@gmail.com>
2021-07-16 10:02:33 -0300

    Atualizando index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 // Creating the router instance
 $router = new \ErickFirmo\Router;
 
-  // Requires helpers
+// Requires helpers
 require __DIR__ . '/../helpers/view.php';
 
 /* Load external routes file */
