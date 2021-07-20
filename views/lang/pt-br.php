7ad179519ad9c97a53521552ff47cb85c0141cbe
Érick Firmo <erickfirmo1996@gmail.com>
2021-07-19 23:01:37 -0300

    Atualizando arquivo de idioma pt-br

--- views/lang/pt-br.php
+++ views/lang/pt-br.php
@@ -1,8 +1,29 @@
 <?php
 
 return [
-    'name' => 'nome',
-    'document_number' => 'cpf',
-    'phone' => 'telefone',
-    'email' => 'e-mail',
+    // attribute names
+    'attributes' => [
+        'name' => 'nome',
+        'email' => 'e-mail',
+        'phone' => 'telefone',
+        'document_number' => 'cpf',
+    ],
+    // errors messages
+    'messages' => [
+        // name
+        'name.required' => 'O nome é obrigatório.',
+        'name.max' => 'O nome deve ter no máximo :max caracteres.',
+        'name.min' => 'O nome deve ter no mínimo :min caracteres.',
+        // email
+        'email.required' => 'O e-mail é obrigatório.',
+        'email.invalid' => 'O e-mail não é válido.',
+        'email.max' => 'O e-mail deve ter no máximo :max caracteres.',
+        'email.min' => 'O e-mail deve ter no mínimo :min caracteres.',
+        // phone
+        'phone.required' => 'O telefone é obrigatório.',
+        'phone.invalid' => 'O telefone é inválido.',
+        // document number
+        'document_number.required' => 'O cpf é obrigatório.',
+        'document_number.invalid' => 'O cpf é inválido.',
+    ]
 ];
