288a1a0173ffa4025d8cef55da0c42b215116691
Érick Firmo <erickfirmo1996@gmail.com>
2021-07-20 13:54:19 -0300

    Atualizando componente de links de paginação

--- views/components/pagination.php
+++ views/components/pagination.php
@@ -1,9 +1,18 @@
 <nav aria-label="Page navigation example">
-  <ul class="pagination">
-  <li class="page-item <?php echo 1 == $_GET['page'] || !isset($_GET['page']) ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="previousPage();">Anterior</a></li>
-    <?php foreach($pages as $page) { ?>
-            <li class="page-item <?php activePage('pessoas', 'active', ['page' => $page]); ?>"><a class="page-link" href="<?php url('pessoas?page='.$page); ?>"><?php echo $page; ?></a></li>
-    <?php } ?>
-    <li class="page-item <?php echo count($pages) == $_GET['page'] || count($pages) == 1 ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="nextPage();">Próxima</a></li>
+    <ul class="pagination">
+    <li class="page-item <?php echo 1 == $_GET['page'] || !isset($_GET['page']) ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="previousPage();">Anterior</a></li>
+    <?php
+      $page = isset($_GET['page']) ? $_GET['page'] : 1;
+      $start = ($page > 5) ? $page - 5 : 1;
+      $end = ($page > 5) ? $page + 4 : 10;
+      $end = count($pages) > 10 ? 10 : count($pages);
+
+      for ($i=$start; $i <= $end; $i++) { 
+        ?>
+          <li class="page-item <?php activePage('pessoas', 'active', ['page' => $i]); ?>"><a class="page-link" href="<?php url('pessoas?page='.$i); ?>"><?php echo $i; ?></a></li>
+        <?php
+      }
+    ?>
+    <li class="page-item <?php echo count($pages) == $_GET['page'] || count($pages) <= 1 ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="nextPage();">Próxima</a></li>
   </ul>
 </nav>
