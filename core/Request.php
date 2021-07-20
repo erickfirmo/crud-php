10a42accc9b200218915d440c9f7a60fa6c0e077
Érick Firmo <erickfirmo1996@gmail.com>
2021-07-19 23:09:32 -0300

    Atualizando objeto Request

--- core/Request.php
+++ core/Request.php
@@ -9,20 +9,23 @@ class Request
     public $lang;
 
     public $errors;
+
+    public $inputs;
     
     public function __construct()
     {
+        // seta idioma da request
         $this->lang = include __DIR__.'/../views/lang/'.app('lang').'.php';
-
-        Session::put('old_fields', $_POST);
     }
     
+    // retorna valor do campo
     public function input($inputName)
     {
-        if(isset($_POST[$inputName]))
+        if(isset($_POST[$inputName])) 
             return $_POST[$inputName];
     }
 
+    // valida campos da requisição
     public function validate(array $rules)
     {
         foreach ($rules as $inputName => $rule)
@@ -40,50 +43,68 @@ class Request
         Session::put('alert-errors', $this->errors);
 
         return $this->status;
-
     }
 
+    // retorna nome do campo no idioma setado
+    public function getInputName($inputName) {
+        return isset($this->lang[$inputName]) ? $this->lang[$inputName] : $inputName;
+    }
+
+    // retorna mensagem de erro do campo no idioma setado
+    public function getMessage($inputRule)
+    {
+        return $this->lang['messages'][$inputRule];
+    }
+
+    // regra para obrigatoriedade do campo
     public function required($inputName, $param=0)
     {
+        $this->inputs[$inputName] = $_POST[$inputName];
+
         if(!isset($_POST[$inputName]) || empty($_POST[$inputName]))
         {
             $this->status = false;
-            $this->errors[$inputName]['required'] = 'O campo '.$this->lang[$inputName].' é obrigatório.';
+            $this->errors[$inputName]['required'] = $this->getMessage($inputName.'.required');
         }
     }
 
+    // regra para máximo de caracteres do valor do campo
     public function max($inputName, $max)
     {
+        $this->inputs[$inputName] = $_POST[$inputName];
+
         if($max < strlen($_POST[$inputName]))
         {
             $this->status = false;
-            $this->errors[$inputName]['max'] = 'O campo '.$this->lang[$inputName].' não deve ter mais que '.$max.' caracteres.';
+            $this->errors[$inputName]['max'] = $this->getMessage($inputName.'.max');
         }
     }
 
+    // regra para mínimo de caracteres do valor do campo
     public function min($inputName, $min)
     {
+        $this->inputs[$inputName] = $_POST[$inputName];
+
         if($min > strlen($_POST[$inputName]))
         {
             $this->status = false;
-            $this->errors[$inputName]['min'] = 'O campo '.$this->lang[$inputName].' deve ter mais que '.$min.' caracteres.';
+            $this->errors[$inputName]['min'] = $this->getMessage($inputName.'.min');
         }  else {
             return false;
         }
     }
     
+    // regra para tipo de dado do valor do campo
     public function datatype($inputName, $type)
     {
+        $this->inputs[$inputName] = $_POST[$inputName];
+
         if($type != gettype($_POST[$inputName]))
         {
             $this->status = false;
-            $this->errors[$inputName]['datatype'] = 'O campo '.$this->lang[$inputName].' deve ser do tipo '.$type.'.';
+            $this->errors[$inputName]['datatype'] = $this->getMessage($inputName.'.datatype');
         }  else {
             return false;
         }
-    }
-
-    public function getInputName($inputName) {
-        return isset($this->lang[$inputName]) ? $this->lang[$inputName] : $inputName;
     }
 }
