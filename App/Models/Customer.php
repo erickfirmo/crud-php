b6bc40faecc072ea76ea6df3f4a76846f9b7320c
Érick Firmo <erickfirmo1996@gmail.com>
2021-07-20 15:03:08 -0300

    Criando método de aquisição de telefone na model Customer

--- App/Models/Customer.php
+++ App/Models/Customer.php
@@ -8,10 +8,16 @@ class Customer extends Model {
 
     public $table = 'customers';
 
-    public $fillables = [
+    public $fillable = [
         'id',
         'name',
         'email',
         'phone'
     ];
+
+    public function getPhone()
+    {
+        $phone= "(".substr($this->phone,0,2).") ".substr($this->phone,2,-4)." -".substr($this->phone,-4);
+        return $phone;
+    }
 }
