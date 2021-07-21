fc56147c9e1dbccece6b40164791c571281ccdbd
Érick Firmo <erickfirmo1996@gmail.com>
2021-07-21 20:27:20 -0300

    Corrigindo final da paginação

--- views/components/pagination.php
+++ views/components/pagination.php
@@ -1,18 +1,31 @@
 <nav aria-label="Page navigation example">
     <ul class="pagination">
       <!-- Voltar página -->
-      <li class="page-item <?php echo 1 == $_GET['page'] || !isset($_GET['page']) || count($pages) <= 1 ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="previousPage();">Anterior</a></li>
+      <li class="page-item <?php echo 1 == $_GET['page'] || !is_numeric($_GET['page']) || !isset($_GET['page']) || count($pages) <= 1 ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="previousPage();">Anterior</a></li>
       <?php
         // verifica se a página foi definida
         $page = isset($_GET['page']) ? $_GET['page'] : 1;
-        // seta número da página de acordo com a quantidade de páginas
-        $page = $page > count($pages) ? count($pages) : $page;
-        // seta primeira página a ser exibida 
-        $start = ($page > 5) ? $page - 5 : 1;
-        // seta última página a ser exibida
-        //$end = ($page > 5) ? $page + 4 : 10;
-        $end = count($pages) < 10 ? count($pages) : 10;
-        $end = $start + ($end - 1);
+
+        if (!is_numeric($page)) {
+          $start = 1;
+          $end = 10;
+        } else {
+          // seta número da página de acordo com a quantidade de páginas
+          $page = $page > count($pages) ? count($pages) : $page;
+          // seta primeira página a ser exibida 
+          $start = ($page > 5) ? $page - 5 : 1;
+          // seta última página a ser exibida
+          $end = count($pages) < 10 ? count($pages) : 10;
+          $end = $start + ($end - 1);
+          // seta array de links a serem exibidos nas 4 ultimas páginas
+          $rest = count($pages) - $page;
+          $rest = $rest < 4 ? $rest : 4;
+          if(count($pages) - $rest <= $page)
+          {
+            $start = $start - (4 - $rest);
+            $end = $end - (4 - $rest);
+          }
+        }
         // percorre página e cria link de páginação
         for ($i=$start; $i <= $end; $i++) { 
           ?>
@@ -21,6 +34,6 @@
         }
       ?>
       <!-- Avançar página -->
-      <li class="page-item <?php echo count($pages) == $_GET['page'] || count($pages) <= 1 ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="nextPage();">Próxima</a></li>
+      <li class="page-item <?php echo count($pages) <= $_GET['page'] || count($pages) < $page ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="nextPage();">Próxima</a></li>
   </ul>
 </nav>
