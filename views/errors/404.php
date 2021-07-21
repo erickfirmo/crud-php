26d8d9407bec3f02fbae7688a284c91279af557b
Érick Firmo <erickfirmo1996@gmail.com>
2021-07-21 15:14:04 -0300

    Criando página de erro 404

---
+++ views/errors/404.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Favicon icon -->
+  <link rel="icon" href="<?php asset('assets/images/favicon.ico'); ?>" type="image/x-icon">
+  <title>CRUD</title>
+  <link rel="stylesheet" media="all" href="<?php asset('assets/css/errors.css'); ?>"/>
+</head>
+<body>
+<div id="clouds">
+            <div class="cloud x1"></div>
+            <div class="cloud x1_5"></div>
+            <div class="cloud x2"></div>
+            <div class="cloud x3"></div>
+            <div class="cloud x4"></div>
+            <div class="cloud x5"></div>
+        </div>
+        <div class='c'>
+            <div class='_404'>404</div>
+            <hr>
+            <!--div class='_1'>A PÁGINA</div-->
+            <div class='_2'>A PÁGINA NÃO FOI ENCONTRADA</div>
+            <a class='btn' href='#'>VOLTAR AO INÍCIO</a>
+        </div>
+</body>
+</html>
