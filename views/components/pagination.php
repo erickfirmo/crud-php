c4b2c99fd2286ca3e8bf7f4d61b00052a0d9bf9f
Érick Firmo <erickfirmo1996@gmail.com>
2021-07-21 13:39:58 -0300

    Melhorias na páginação

--- views/components/pagination.php
+++ views/components/pagination.php
@@ -1,18 +1,26 @@
 <nav aria-label="Page navigation example">
     <ul class="pagination">
-    <li class="page-item <?php echo 1 == $_GET['page'] || !isset($_GET['page']) || count($pages) <= 1 ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="previousPage();">Anterior</a></li>
-    <?php
-      $page = isset($_GET['page']) ? $_GET['page'] : 1;
-      $start = ($page > 5) ? $page - 5 : 1;
-      $end = ($page > 5) ? $page + 4 : 10;
-      $end = count($pages) > 10 ? 10 : count($pages);
-
-      for ($i=$start; $i <= $end; $i++) { 
-        ?>
-          <li class="page-item <?php activePage('pessoas', 'active', ['page' => $i]); ?>"><a class="page-link" href="<?php url('pessoas?page='.$i); ?>"><?php echo $i; ?></a></li>
-        <?php
-      }
-    ?>
-    <li class="page-item <?php echo count($pages) == $_GET['page'] || count($pages) <= 1 ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="nextPage();">Próxima</a></li>
+      <!-- Voltar página -->
+      <li class="page-item <?php echo 1 == $_GET['page'] || !isset($_GET['page']) || count($pages) <= 1 ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="previousPage();">Anterior</a></li>
+      <?php
+        // verifica se a página foi definida
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+        // seta número da página de acordo com a quantidade de páginas
+        $page = $page > count($pages) ? count($pages) : $page;
+        // seta primeira página a ser exibida 
+        $start = ($page > 5) ? $page - 5 : 1;
+        // seta última página a ser exibida
+        //$end = ($page > 5) ? $page + 4 : 10;
+        $end = count($pages) < 10 ? count($pages) : 10;
+        $end = $start + ($end - 1);
+        // percorre página e cria link de páginação
+        for ($i=$start; $i <= $end; $i++) { 
+          ?>
+            <li class="page-item <?php activePage('pessoas', 'active', ['page' => $i]); ?>"><a class="page-link" href="<?php url('pessoas?page='.$i); ?>"><?php echo $i; ?></a></li>
+          <?php
+        }
+      ?>
+      <!-- Avançar página -->
+      <li class="page-item <?php echo count($pages) == $_GET['page'] || count($pages) <= 1 ? 'disabled' : ''; ?>"><a class="page-link" href="javascript:void(0);" onclick="nextPage();">Próxima</a></li>
   </ul>
 </nav>
