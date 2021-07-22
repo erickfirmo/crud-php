2b7c3c83fa839e253364747080ece154b3ba1164
Érick Firmo <erickfirmo1996@gmail.com>
2021-07-22 00:55:54 -0300

    Atualizando home

--- views/home.php
+++ views/home.php
@@ -77,8 +77,8 @@
                                 <div class="row align-items-center">
                                     <div class="col-md-8">
                                         <div class="page-header-title">
-                                            <h5 class="m-b-10">Pessoas</h5>
-                                            <p class="m-b-0">Listar Todas</p>
+                                            <h5 class="m-b-10">Dashboard</h5>
+                                            <p class="m-b-0">Home</p>
                                         </div>
                                     </div>
                                     <div class="col-md-4">
@@ -86,9 +86,7 @@
                                             <li class="breadcrumb-item">
                                                 <a href="<?php url('/'); ?>"> <i class="fa fa-home"></i> </a>
                                             </li>
-                                            <li class="breadcrumb-item"><a href="<?php url('pessoas'); ?>">Pessoas</a>
-                                            </li>
-                                            <li class="breadcrumb-item"><a href="<?php url('pessoas'); ?>">Listar Todas</a>
+                                            <li class="breadcrumb-item"><a href="<?php url('pessoas'); ?>">Home</a>
                                             </li>
                                         </ul>
                                     </div>
