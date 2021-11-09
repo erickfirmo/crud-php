408cc7d9ce7c8f56414699d73322ab37037add27
Érick Firmo <erickfirmo1996@gmail.com>
2021-11-09 13:13:01 -0300

    Updating index.php

--- public/index.php
+++ public/index.php
@@ -1,22 +1,32 @@
 <?php
 
-// Requires composer autoloader
+#
+ini_set('display_errors', 1);
+
+#
+ini_set('display_startup_errors', 1);
+
+# 
+error_reporting(E_ALL);
+
+# Requires composer autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
-// Iniciando session
+# Iniciando session
 \Core\Session::start();
 
-// Creating the router instance
+# Creating the router instance
 $router = new \ErickFirmo\Router;
 
-// Defining erro page 404
+# Defining erro page 404
 $router->notFoundView(__DIR__.'/../views/errors/404.php');
 
-// Requires helpers
+# Requires helpers
 require __DIR__ . '/../helpers/functions.php';
 
-// Load external routes file 
-require __DIR__.'/../routes/app.php';
+# Load external routes file 
+require __DIR__.'/../routes/web.php';
 
-// Run the router
-$router->run();  
+# Run the router and creating the request 
+$router->run(new \Core\Request);
+
