91c715e8fc9a00ce7e9a5ef05b85934f174e1d43
Érick Firmo <erickfirmo1996@gmail.com>
2021-11-11 19:32:19 -0300

    Updating application start in index.php file

--- public/index.php
+++ public/index.php
@@ -15,18 +15,31 @@ require __DIR__ . '/../vendor/autoload.php';
 # Iniciando session
 \Core\Session::start();
 
+# Requires helpers
+require __DIR__ . '/../helpers/functions.php';
+
 # Creating the router instance
 $router = new \ErickFirmo\Router;
 
-# Defining erro page 404
-$router->notFoundView(__DIR__.'/../views/errors/404.php');
+# Defining generic error page 
+$ambience = 'local';
 
-# Requires helpers
-require __DIR__ . '/../helpers/functions.php';
+# Instanciando kernel
+$kernel = new \Core\Kernel();
+
+# Configurando página de errors (prod)
+$kernel->errorsPage(__DIR__.'/../views/errors/500.php');
+
+# Configurando página de errors (dev/local)
+if('prod' != $ambience) {
+    $kernel->errorsPage(__DIR__.'/../views/errors/exceptions.php');
+}
+
+# Defining error page 404
+$router->notFoundView(__DIR__.'/../views/errors/404.php');
 
 # Load external routes file 
 require __DIR__.'/../routes/web.php';
 
 # Run the router and creating the request 
-$router->run(new \Core\Request);
-
+$kernel->handle($router);
