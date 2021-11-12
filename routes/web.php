7ef0e2fc9fdae8c2955aa935389a6cbe13563225
Érick Firmo <erickfirmo1996@gmail.com>
2021-11-12 04:40:02 -0300

    Updating web routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 $router->get('/', HomeController::class, 'index', 'home');
 $router->get('/pessoas', CustomerController::class, 'index');
 $router->get('/pessoas/cadastrar', CustomerController::class, 'create', 'customers.create');
-$router->get('/pessoas/editar/{$id}', CustomerController::class, 'edit');
+$router->get('/pessoas/editar/{id}', CustomerController::class, 'edit');
 $router->get('/pessoas/{id}', CustomerController::class, 'show', 'customers.show');
-$router->post('/pessoas/store', CustomerController::class, 'store');
+$router->post('/pessoas/store', CustomerController::class, 'store', 'store');
 $router->put('/pessoas/update/{id}', CustomerController::class, 'update');
 $router->delete('/pessoas/destroy/{id}', CustomerController::class, 'destroy');
