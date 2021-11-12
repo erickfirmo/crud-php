9b9faace413f53ee8bf82a2ca34656b77c21f27f
Érick Firmo <erickfirmo1996@gmail.com>
2021-11-12 04:38:41 -0300

    Updating exceptions error page

--- views/errors/exceptions.php
+++ views/errors/exceptions.php
@@ -6,23 +6,14 @@
   <!-- Favicon icon -->
   <link rel="icon" href="<?php asset('assets/images/favicon.ico'); ?>" type="image/x-icon">
   <title>CRUD</title>
-  <link rel="stylesheet" media="all" href="<?php asset('assets/css/errors.css'); ?>"/>
+  <style>
+    .title {
+      color: #ff0000;
+      text-align: center;
+    }
+  </style>
 </head>
 <body>
-<div id="clouds">
-            <div class="cloud x1"></div>
-            <div class="cloud x1_5"></div>
-            <div class="cloud x2"></div>
-            <div class="cloud x3"></div>
-            <div class="cloud x4"></div>
-            <div class="cloud x5"></div>
-        </div>
-        <div class='c'>
-            <div class='_404'>500</div>
-            <hr>
-            <!--div class='_1'>A PÁGINA</div-->
-            <div class='_2'><?php echo $error; ?></div>
-            <!--a class='btn' href='<?php #url('/'); ?>'>VOLTAR AO INÍCIO</a-->
-        </div>
+  <h1 class="title">500 - <?php echo $error; ?></h1>
 </body>
 </html>
